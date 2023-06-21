0536e726f58b415fbe92b6117114c97dbdf1fca7
unknown <rakesh.xworkz@gmail.com>
2023-06-21 11:53:25 +0530

    arrays

---
+++ Arraysclass.java
@@ -0,0 +1,40 @@
+public class Arraysclass{
+	public static void main(String[] args){
+		int [] numberofGroups = {10,15,20,25,};
+		System.out.println( numberofGroups[0]);
+		int [] numberofBranchs = {2,4,6,};
+		System.out.println(numberofBranchs [2]);
+		int [] numberofBooks = {10, 20, 30,25};
+		System.out.println(numberofBooks[3]);
+		for (int i=2; i<=4; i++){
+			System.out.println(numberofBooks[i]);
+			int [] numberofPens = {2,4,6,8,5};
+			System.out.println(numberofPens[4]);
+		int [] numberofCountries = {5,10};
+		System.out.println(numberofCountries[0]);
+		int [] numberofRivers = {3,6,9};
+		System.out.println( numberofRivers[1]);
+		char [] name = {'r','a','j'};
+		System.out.println(name[0]);
+		char [] vowels = {'a','e','i','o','u'};
+		System.out.println(vowels[4]);
+		float [] sgpaofStudents = {7.8f,8.5f,9.6f,8.4f};
+		System.out.println(sgpaofStudents[3]);
+		float [] gravityofEarth = {9.1f,8.1f,};
+		System.out.println( gravityofEarth[1]);
+		
+		
+			
+		}
+			
+		
+		
+		
+		
+			
+		}
+		
+				
+			
+		
+	}
