5730bf02f886a243349b02ed7a6ed241be97fca2
unknown <rakesh.xworkz@gmail.com>
2023-07-07 09:10:09 +0530

    motor

--- Motor.java
+++ Motor.java
@@ -1,71 +1,53 @@
-public class Motor {
-    // States
-    public String motorNumber;
-    public String motorType;
-    public double voltage;
-    public double current;
-    public boolean isMotorRunning;
-    public boolean isMotorFunctional;
-    public double motorTemperature;
-    public double motorSpeed;
-    public String manufacturer;
-    public String installationDate;
+public class Motor{
+		static {
+		System.out.println("Static Block");
+		int inputVoltage=240;
+		int frequency=50;
+		int ratedPower=60;
+		float ratedTorque=0.4f;
+		motorType="AC motor";
+		
+		
+		}
+	
+	 //static variables 
+	static String motorNumber;
+	static String motorType;
+	static double voltage;
+	static double current;
+	static double motorSpeed;
+	
+	// nonstatic variables 
+	boolean isMotorFunctional;
+	boolean isMotorRunning;
+	String color;
+	int price;
+	String manufacture;
 
-    // Constructor
-    public Motor(String motorNumber, String motorType, double voltage, double current, boolean isMotorRunning, String manufacturer) {
-        this.motorNumber = motorNumber;
-        this.motorType = motorType;
-        this.voltage = voltage;
-        this.current = current;
-        this.isMotorRunning = isMotorRunning;
-        this.manufacturer = manufacturer;
-    }
+	
+	public Motor(boolean isMotorFunctional, boolean isMotorRunning, String color,int price,String manufacture){
+		System.out.println("Printing nonStatic variables");
+		this.isMotorFunctional=isMotorFunctional;
+		this.isMotorRunning=isMotorRunning;
+		this.color=color;
+		this.price=price;
+		this.manufacture=manufacture;
+		
+	}
+	public static void main(String[] args){
+		Motor motor= new Motor(true, false,"Red", 15000, "ABC motors");
+		System.out.println(motor.isMotorFunctional);
+		System.out.println(motor.isMotorRunning);
+		System.out.println(motor.color);
+		System.out.println(motor.price);
+		System.out.println(motor.manufacture);
 
-    // Behaviors
-    public void startMotor() {
-       
-    }
-
-    public void stopMotor() {
-    
-    }
-
-    public void checkMotorStatus() {
- 
-    }
-
-    public void increaseMotorSpeed() {
-       
-    }
-
-    public void decreaseMotorSpeed() {
-       
-    }
-
-    public void checkMotorTemperature() {
-        
-    }
-
-    public void changeMotorType() {
-      
-    }
-
-    public void updateManufacturer() {
-      
-    }
-
-    public void updateInstallationDate() {
-      
-    }
-
-    public static void main(String[] args) {
-        Motor motor = new Motor("M123", "Electric", 220.0, 5.0, false, "ABC Motors");
-        System.out.println(motor);
-        System.out.println(motor.motorNumber);
-        System.out.println(motor.motorType);
-        System.out.println(motor.voltage);
-        System.out.println(motor.current);
-        System.out.println(motor.isMotorRunning);
-        System.out.println(motor.manufacturer);
-    }
+		
+	
+		
+	}
+	
 }
+
+
+
