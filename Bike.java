bf5828a37c57bc68232ad9976d09d6ebd8e3d5d2
unknown <rakesh.xworkz@gmail.com>
2023-07-07 18:26:07 +0530

    test

---
+++ Bike.java
@@ -0,0 +1,20 @@
+public class Bike{
+  string name;
+  int number;
+  public Bike(string name,int number){
+	  this.name = name;
+	  this.number= number;
+  }
+  public String name(){
+	  return name;
+  }
+  public int number(){
+	  return number;
+  }
+  public static void main(String[] args){
+	  Bike myBike= new bike("hero",42);
+	  
+	  System.out.println(mybike.name();
+	   System.out.println(mybike.number();
+  }
+}
